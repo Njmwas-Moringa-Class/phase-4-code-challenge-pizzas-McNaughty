de7aed719f7b3abd8177e71736dd27868ab857cd
Winnie <wimacoj@gmail.com>
2024-03-09 11:10:17 +0300

    Endpoints

--- server/app.py
+++ server/app.py
@@ -6,6 +6,8 @@ from flask import Flask, request, make_response
 from flask_restful import Api, Resource
 import os
 
+from flask import jsonify
+
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE = os.environ.get(
     "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
@@ -19,15 +21,20 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+restaurants =  [
+  { "address": "address1", "id": 1, "name": "Karen's Pizza Shack" },
+  { "address": "address2", "id": 2, "name": "Sanjay's Pizza"  },
+  { "address": "address3", "id": 3,  "name": "Kiki's Pizza"  }
+]
+
 
 @app.route('/')
 def index():
     return '<h1>Code challenge</h1>'
 
-@app.route('/restaurants')
-def restaurant():
-    print ('Working')
-    return 'Working'
+@app.route('/restaurants', methods=['GET'])
+def list_restaurants():
+    return  jsonify(restaurants)
 
 
 if __name__ == '__main__':
